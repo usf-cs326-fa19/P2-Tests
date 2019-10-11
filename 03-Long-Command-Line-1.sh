9744394fc46819024e51105d8d54f4460073298a
Matthew Malensek <matt@malensek.net>
2019-10-11 00:26:28 -0700

    Long command line test

---
+++ 03-Long-Command-Line-1.sh
@@ -0,0 +1,16 @@
+source "${TEST_DIR}/funcs.bash"
+
+run_timeout=20
+reference_run sh < "${TEST_DIR}/scripts/long.sh"
+
+test_start "Long command lines" \
+    "Your shell should ignore arguments if too many were given."
+
+# Ignore log messages because they're going to be huge if the line is printed:
+run ./$SHELL_NAME < "${TEST_DIR}/scripts/too-long.sh" 2> /dev/null
+
+run ./$SHELL_NAME < "${TEST_DIR}/scripts/long.sh"
+
+compare <(echo "${expected}") <(echo "${actual}")
+
+test_end
