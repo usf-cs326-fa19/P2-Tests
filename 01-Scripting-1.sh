b0a8cb56997c5ec5a27b27af2c8ea7d18e49401e
Matthew Malensek <matt@malensek.net>
2019-10-10 23:44:21 -0700

    Add timeout

--- 01-Scripting-1.sh
+++ 01-Scripting-1.sh
@@ -1,12 +1,11 @@
 source "${TEST_DIR}/funcs.bash"
+run_timeout=2
+reference_run sh < "${TEST_DIR}/scripts/inspector.sh" 2> /dev/null
 
 test_start "Scripting Support" \
     "Trailing spaces should be eliminated by your command line tokenization process"
 
-reference_run sh < "${TEST_DIR}/scripts/inspector.sh" 2> /dev/null
-
 run ./$SHELL_NAME < "${TEST_DIR}/scripts/inspector.sh" 2> /dev/null
-
 compare <(echo "${expected_output}") <(echo "${program_output}")
 
 test_end
